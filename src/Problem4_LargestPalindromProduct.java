4bbdf664e546366b7f6063978a085dac88d70868
Luisa <massimo.e.luisa.au@gmail.com>
2021-03-24 15:59:55 +0100

    Euler4

--- src/Problem4_LargestPalindromProduct.java
+++ src/Problem4_LargestPalindromProduct.java
@@ -2,29 +2,39 @@ import java.util.Arrays;
 
 public class Problem4_LargestPalindromProduct {
     public static void main(String[] args) {
-
+        int myNumber= 1234321;
+        String myNumInStr ;
+        myNumInStr= Integer.toString(myNumber);
         System.out.println("findLargestPal() = " + findLargestPal());
+        //System.out.println("isPal(my) = " + isPal(myNumInStr));
     }
 
     public static int findLargestPal(){
-        int palNumber=0;
-        int myNumber=0;
-        for (int i =100; i < 1000; i++) {
+        int biggestPalNumber=0;
+        int myNumber;
+        String myNumInStr ;
+        //isPal(myNumber);
+       for (int i =100; i < 1000; i++) {
             for (int j = 100; j <1000 ; j++) {
                 myNumber=i*j;
-                 if( isPal(myNumber)){
-                     palNumber=myNumber;
+                 myNumInStr= Integer.toString(myNumber);
+                 if( isPal(myNumInStr)){
+                     biggestPalNumber=myNumber;
                  }
             }
-
         }
 
-        return palNumber;
+        return biggestPalNumber;
     }
 
-    public static boolean isPal(int myNumber){
-        int[] arr=new int[6];
-//TODO    //es fehlt alles
+    public static boolean isPal(String myNumber){
+        if(myNumber.length()==0 || myNumber.length()==1) {   //stop condition
+            return true;
+        }
+        if(myNumber.charAt(0)==myNumber.charAt(myNumber.length()-1)){
+            String subString =myNumber.substring(1,myNumber.length()-1);
+            return isPal(subString);       //recursive call
+        }
         return false;
     }
 
